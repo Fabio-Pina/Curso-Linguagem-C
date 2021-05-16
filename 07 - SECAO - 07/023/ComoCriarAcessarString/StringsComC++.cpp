2b180b914c80010738c09ff7ef3ae7947fb6dac9
Fabio-Pina <fabiopina422@gmail.com>
2021-05-15 23:12:49 -0300

    Enviando sétima e última seção

---
+++ 07 - SECAO - 07/023/ComoCriarAcessarString/StringsComC++.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// Função principal do programa
+int main() {
+
+    // Definindo uma String
+    string palavra;
+
+    // Imprimindo na tela
+    cout << "Digite uma palavra";
+
+    // Lendo uma string
+    cin >> palavra;
+
+    // Imprimindo uma variavel
+    cout << "\nA palavra é: " << palavra;
+
+    return 0;
+}
